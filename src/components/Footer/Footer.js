d0c6dc3e507f13c0d5580bb8095034708f5a213b
EliteDev0227 <59699555+EliteDev0227@users.noreply.github.com>
2021-01-31 08:40:51 +0800

    Added material ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import twitterIcon from '../../assets/twitter.svg';
-import facebookIcon from '../../assets/facebook.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import instagramIcon from '../../assets/instagram.svg';
+import facebookIcon from '../../assets/facebook.svg';
+import twitterIcon from '../../assets/twitter.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 
 const Wrapper = styled.div`
   background-color: black;
@@ -16,15 +32,58 @@ display: flex;
 column-gap: 10px;
 `;
 
-const Footer = () => (
-  <Wrapper>
-    <div className="text-title color-white">Follow Us</div>
-    <IconWrapper>
-      <img className="icon" src={facebookIcon} />
-      <img className="icon" src={instagramIcon} />
-      <img className="icon" src={twitterIcon} />
-    </IconWrapper>
-  </Wrapper>
-);
+const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <Wrapper>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
+      <div className="text-title color-white">Follow Us</div>
+      <IconWrapper>
+        <img className="icon" src={facebookIcon} />
+        <img className="icon" src={instagramIcon} />
+        <img className="icon" src={twitterIcon} />
+      </IconWrapper>
+    </Wrapper>
+  );
+};
 
 export default Footer;
